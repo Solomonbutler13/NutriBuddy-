7e18ab8d5ac6ab1f2e2542a3d70a2e9e60f12c85
Solomon <solomonbutler13sb@gmail.com>
2024-04-11 21:12:47 -0400

    new

---
+++ Avatar.jsx
@@ -0,0 +1,22 @@
+
+
+// Create Avatar Component in App (<Avatar/>)
+// Create an “import Avatar from “.Avatar” ”
+// Create Avatar.jsx  / .css page
+
+
+// Pseudocode For Avatar
+
+// Function to load avatar options from the API
+
+// Make an API call to fetch avatars
+
+// Function to populate select dropdown with avatar options ( assuming each has an id )
+
+// Append each to options
+
+// Function to update the avatar display
+
+// Create API call function (using URL)
+
+// Function to get avatar image URL by ID
