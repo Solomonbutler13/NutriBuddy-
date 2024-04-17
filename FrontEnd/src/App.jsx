b15d885ad42cb1d141dd50d04012dcd2b44726cb
denniswynn22 <147005838+denniswynn22@users.noreply.github.com>
2024-04-17 11:39:13 -0400

    Auth0 component added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './index.css'
 
 
@@ -15,6 +17,51 @@ import MealPlanPanel from './pages/meal_plan_page/MealPlanPanel.jsx'
 
 
 export default function App() {
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  // Loading State
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  // Error State
+  if (error) {
+    return <div>Oops...wrong fridge {error.message}</div>
+  }
+
+  // Content to render based on authentication state
+  const renderContent = () => {
+    if (isAuthenticated) {
+      // User is authenticated
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<PersonalPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/personal_info" element={<PersonalPage />} />
+          </Routes>
+        </Router>
+      );
+    } else {
+      // User is not authenticated
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/personal_info" element={<Login redirectUrl="/personal_info" />} />
+          </Routes>
+        </Router>
+      );
+    }
+  };
 
   return (
     <>
@@ -60,5 +107,5 @@ export default function App() {
       </Routes>
     </Router>
     </>
-  )
+  );
 }
