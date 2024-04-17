53e90c9818ff4f34d9bdc6daf3fd767cffe3ba45
denniswynn22 <147005838+denniswynn22@users.noreply.github.com>
2024-04-17 11:39:13 -0400

    Auth0 component added

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,64 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './index.css'
 
 // Importing all the component pages here as we make them
-import Login from './pages/login.jsx'
-import SignUp from './pages/signup.jsx'
+import Login from './components/login_page/login.jsx'
+// import SignUp from './pages/signup.jsx'
+// import PersonalPage missing
 
 export default function App() {
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    user,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  // Loading State
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  // Error State
+  if (error) {
+    return <div>Oops...wrong fridge {error.message}</div>
+  }
+
+  // Content to render based on authentication state
+  const renderContent = () => {
+    if (isAuthenticated) {
+      // User is authenticated
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<PersonalPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/personal_info" element={<PersonalPage />} />
+          </Routes>
+        </Router>
+      );
+    } else {
+      // User is not authenticated
+      return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/personal_info" element={<Login redirectUrl="/personal_info" />} />
+          </Routes>
+        </Router>
+      );
+    }
+  };
 
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-      </Routes>
-    </Router>
+      {renderContent()}
     </>
-  )
+  );
 }
