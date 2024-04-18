f227cb322fa5fc3c14f5073cf1ae35318520eef9
denniswynn22 <147005838+denniswynn22@users.noreply.github.com>
2024-04-18 19:48:32 -0400

    redirect still not working

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -15,7 +15,6 @@ export default function App() {
     isAuthenticated,
     error,
     user,
-    loginWithRedirect,
     logout,
   } = useAuth0();
 
@@ -23,12 +22,7 @@ export default function App() {
   if (isLoading) {
     return <div>Loading...</div>
   }
-
-  // Error State
-  if (error) {
-    return <div>Oops...wrong fridge {error.message}</div>
-  }
-
+  
   // Content to render based on authentication state
   const renderContent = () => {
     if (isAuthenticated) {
