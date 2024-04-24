060bc1c58e6788695082c3c92e1b32bb1becd80a
Jason2945 <wj2945@gmail.com>
2024-04-24 19:22:59 -0400

    Finished front end

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -13,7 +13,10 @@ import MealPlanPanel from './pages/meal_plan_page/MealPlanPanel.jsx'
 import Login from './pages/login_page/login.jsx';
 import SignUp from './pages/signup_page/signup.jsx';
 import PersonalPage from './pages/personal_page/personal_info.jsx';
+import Diet from './pages/diet_page/diet_info.jsx';
 import Test from './pages/test.jsx';
+import Allergies from './pages/allergies_page/allergies_info.jsx';
+import Activity from './pages/activity_page/activity_info.jsx';
 
 export default function App() {
 
@@ -54,6 +57,9 @@ export default function App() {
           },
           ]} />} />
           <Route path='/about' element = {<AboutNutriBuddy />} />
+        <Route path='/diet_info' element={<Diet />} />
+        <Route path='/allergies_info' element={<Allergies />} />
+        <Route path='/activity_info' element={<Activity />} />
         <Route path='/test' element={<Test />} />
       </Routes>
     </Router>
