e3faf955936f469879bba50d16366e1909d8a59f
Brandon @ Nebula <brandon@nebulaacademy.org>
2024-04-27 11:32:26 -0400

    Fix callback URL bug in login page and refactor to use Auth0 login

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: 'http://localhost:5174/callback',
+        redirect_uri: 'http://localhost:5173/callback',
       }}
     >
 
