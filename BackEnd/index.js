018458197d544754c6a65ef6fb01b71dbc0fc7e2
Jason2945 <wj2945@gmail.com>
2024-05-13 19:38:06 -0400

    added more queries

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -24,6 +24,12 @@ app.get('/users/all', db.getAllUsers);
 app.get('/users/:id', db.getUserById);
 // This deletes the user from the database
 app.delete('/users/del/:id', db.deleteUserById);
+// This updates user personal info in database
+app.put('/users/updatepersonal/:id', db.updateUserPersonalById);
+// This updates user allergy ino in database
+app.put('/users/updateallergies/:id', db.updateUserAllergiesById);
+// This allows users to add user
+app.post('/users/add', db.addUser);
 
 // This gets the all the meals for a user through user id
 app.get('/usermeals/:id', db.getUserMealsByUserId)
@@ -35,9 +41,15 @@ app.get('/usermeals/currdate/:id/:date', db.getUsersMealsByFutureDate)
 app.get('/usermeals/mealtype/:id/:type', db.getUsersMealsByMealType)
 // This gets all the meals for a user through calories
 app.get('/usermeals/calories/:id/:calories', db.getUsersMealsByCalories)
+// This allows users to add user meal
+app.post('/usermeals/add', db.addUserMeal);
+// This allows users to del user meal
+app.delete('/usermeals/del/:id', db.deleteUserMealById);
 
 // This allows users to get the ingredients for a meal
 app.get('/mealingredients/:id', db.getIngredientsByMealId);
+// This allows devs to add ingredients for a meal
+app.post('/mealingredients/add', db.addIngredients);
 
 // This gets the users favorite meal
 app.get('/favoritemeals/:id', db.getFavMealsById);
--- BackEnd/queries.js
+++ BackEnd/queries.js
@@ -52,6 +52,136 @@ const deleteUserById = (request, response) => {
         response.status(200).json(results.rows);
     });
 };
+
+const updateUserPersonalById = (request, response) => {
+    let { id, first_name, last_name, age, email, curr_weight, goal_weight, height, calorie_goal} = request.body;
+    let myPromise = new Promise(function(resolve, reject){
+        pool.query(`SELECT * FROM users WHERE id=$1`, [id], (error, results) => {
+            if (error){
+                throw error;
+            } else if(response){
+                first_name = first_name !== undefined ? first_name : results.rows.first_name;
+                last_name = last_name !== undefined ? last_name : results.rows.last_name;
+                age = age !== undefined ? age : results.rows.age;
+                email = email !== undefined ? email : results.rows.email;
+                curr_weight = curr_weight !== undefined ? curr_weight : results.rows.curr_weight;
+                goal_weight = goal_weight !== undefined ? goal_weight : results.rows.goal_weight;
+                height = height !== undefined ? height : results.rows.height;
+                calorie_goal = calorie_goal !== undefined ? calorie_goal : results.rows.calorie_goal;
+                resolve(results.rows)
+                return results.rows
+            }else {
+                reject()
+            }
+        })
+    })
+
+    myPromise.then(() => {
+        try {
+            pool.query(
+                `UPDATE users
+                    SET first_name=$1, last_name=$2, age=$3, email=$4,
+                    curr_weight=$5, goal_weight=$6, height=$7, calorie_goal=$8`,
+                [first_name, last_name, age, email, curr_weight, goal_weight, height, calorie_goal],
+                (error, results) => {
+                    if (error) {
+                        console.log(error, '<--- error here')
+                        throw error;
+                    }
+                    console.log(results, "<--- results")
+                    response.status(200).json(results.rows)
+                }
+            );
+        }catch (error) {
+            throw error;
+        }
+    })
+};
+
+const updateUserAllergiesById = (request, response) => {
+    let { id, fish, soy, milk, shellfish, nuts, eggs, wheat, sesame} = request.body;
+    let myPromise = new Promise(function(resolve, reject){
+        pool.query(`SELECT * FROM users WHERE id=$1`, [id], (error, results) => {
+            if (error){
+                throw error;
+            } else if(response){
+                fish = fish !== undefined ? fish : results.rows.fish;
+                soy = soy !== undefined ? soy : results.rows.soy;
+                milk = milk !== undefined ? milk : results.rows.milk;
+                shellfish = shellfish !== undefined ? shellfish : results.rows.shellfish;
+                nuts = nuts !== undefined ? nuts : results.rows.nuts;
+                eggs = eggs !== undefined ? eggs : results.rows.eggs;
+                wheat = wheat !== undefined ? wheat : results.rows.wheat;
+                sesame = sesame !== undefined ? sesame : results.rows.sesame;
+                resolve(results.rows)
+                return results.rows
+            }else {
+                reject()
+            }
+        })
+    })
+
+    myPromise.then(() => {
+        try {
+            pool.query(
+                `UPDATE users
+                    SET fish=$1, soy=$2, milk=$3, shellfish=$4,
+                    nuts=$5, eggs=$6, wheat=$7, sesame=$8`,
+                [fish, soy, milk, shellfish, nuts, eggs, wheat, sesame],
+                (error, results) => {
+                    if (error) {
+                        console.log(error, '<--- error here')
+                        throw error;
+                    }
+                    console.log(results, "<--- results")
+                    response.status(200).json(results.rows)
+                }
+            );
+        }catch (error) {
+            throw error;
+        }
+    })
+};
+
+const addUser = (request, response) => {
+    try {
+        const {
+            first_name,
+            last_name,
+            age,
+            email,
+            curr_weight,
+            goal_weight,
+            height,
+            calorie_goal,
+            fish,
+            soy,
+            milk,
+            shellfish,
+            nuts,
+            eggs,
+            wheat,
+            sesame
+        } = request.body;
+        pool.query(
+            `INSERT INTO users (first_name, last_name, age, email, curr_weight, goal_weight,
+            height, calorie_goal, fish, soy, milk, shellfish, nuts, eggs, wheat, sesame) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING *`,
+            [first_name, last_name, age, email, curr_weight, goal_weight, height, calorie_goal,
+            fish, soy, milk, shellfish, nuts, eggs, wheat, sesame],
+            (error, results) => {
+                if (error) {
+                    console.log(error, '<--- error here')
+                    throw error;
+                }
+                response.status(201).json(results.rows)
+            }
+        );
+    } catch (error) {
+        throw error;
+    }
+};
+
 //////////////////////////////////////////////////////////////////////////////////
 // Meals Table Query
 const getUserMealsByUserId = (request, response) => {
@@ -107,6 +237,43 @@ const getUsersMealsByCalories = (request, response) => {
         response.status(200).json(results.rows);
     });
 };
+
+const addUserMeal = (request, response) => {
+    try {
+        const {
+            recipe_id,
+            user_id,
+            meal_name,
+            meal_time,
+            meal_type,
+            meal_calories
+        } = request.body;
+        pool.query(
+            `INSERT INTO meal (recipe_id, user_id, meal_name, meal_time, meal_type, meal_calories)
+             VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+            [recipe_id, user_id, meal_name, meal_time, meal_type, meal_calories],
+            (error, results) => {
+                if (error) {
+                    console.log(error, '<--- error here')
+                    throw error;
+                }
+                response.status(201).json(results.rows)
+            }
+        );
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteUserMealById = (request, response) => {
+    const id = parseInt(request.params.id);
+    pool.query(`DELETE FROM meal WHERE id = ${id}`, (error, results) => {
+        if(error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
 //////////////////////////////////////////////////////////////////////////////////
 // Ingredients Table Query
 const getIngredientsByMealId = (request, response) => {
@@ -117,6 +284,32 @@ const getIngredientsByMealId = (request, response) => {
         }
         response.status(200).json(results.rows);
     });
+};
+
+const addIngredients = (request, response) => {
+    try {
+        const {
+            recipe_id,
+            ingredient_name,
+            quantity,
+            unit_type,
+            price_per_unit
+        } = request.body;
+        pool.query(
+            `INSERT INTO ingredients (recipe_id, ingredient_name, quantity, unit_type, price_per_unit)
+             VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+            [recipe_id, ingredient_name, quantity, unit_type, price_per_unit],
+            (error, results) => {
+                if (error) {
+                    console.log(error, '<--- error here')
+                    throw error;
+                }
+                response.status(201).json(results.rows)
+            }
+        );
+    } catch (error) {
+        throw error;
+    }
 };
 //////////////////////////////////////////////////////////////////////////////////
 // Favorite Table Query
@@ -167,14 +360,20 @@ module.exports = {
     getAllUsers,
     getUserById,
     deleteUserById,
+    updateUserPersonalById,
+    updateUserAllergiesById,
+    addUser,
 
     getUserMealsByUserId,
     getUsersMealsByPastDate,
     getUsersMealsByFutureDate,
     getUsersMealsByMealType,
     getUsersMealsByCalories,
+    addUserMeal,
+    deleteUserMealById,
 
     getIngredientsByMealId,
+    addIngredients,
 
     getFavMealsById,
     addFavMeal,
