f9591f78324e9fe6ba7f68c667044bada958327c
didarkhan97 <didark791@gmail.com>
2024-05-31 21:15:12 -0400

    completed mealplan

--- FrontEnd/src/pages/meal_plan_page/MealPlanPanel.jsx
+++ FrontEnd/src/pages/meal_plan_page/MealPlanPanel.jsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MealCard from '../../components/MealCard';
 import WeeklyPlan from '../../components/WeeklyPlan';
